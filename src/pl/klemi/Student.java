804fde32f904b1c4943c7aa9cfa6fbcbf740952d
Kamil <kamil@klemczak.pl>
2022-09-01 00:39:48 +0200

    Dziennik

---
+++ src/pl/klemi/Student.java
@@ -0,0 +1,13 @@
+package pl.klemi;
+
+import java.time.LocalDate;
+
+public class Student {
+    private String name;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String gender;
+
+
+
+}
---
+++ src/pl/klemi/Teacher.java
@@ -0,0 +1,10 @@
+package pl.klemi;
+
+import java.time.LocalDate;
+
+public class Teacher {
+    private String name;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String gender;
+}
